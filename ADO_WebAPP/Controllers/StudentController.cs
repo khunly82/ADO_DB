6d499b8215bb318ff74d8c100959c0f9b807fb7a
khun <lykhun@gmail.com>
2024-01-31 17:00:09 +0100

    asp

---
+++ ADO_WebAPP/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using ADO_Console.Domain;
+using ADO_Console.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ADO_WebAPP.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            SqlConnection connection = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=TFDemoADO;integrated security=true");
+            StudentRepository repo = new StudentRepository(connection);
+
+            return View(repo.Get());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            SqlConnection connection = new SqlConnection("server=(localdb)\\MSSQLLocalDB;database=TFDemoADO;integrated security=true");
+            StudentRepository repo = new StudentRepository(connection);
+
+            repo.Add(student);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
